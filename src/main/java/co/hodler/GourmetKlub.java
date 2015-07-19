930a7b69fb8c5e8479b1fcdaf8cc910474a625a1
xorrr <derxor@gmail.com>
2015-07-19 21:21:54 +0200

    use PORT env var as the port to run the server on

--- src/main/java/co/hodler/GourmetKlub.java
+++ src/main/java/co/hodler/GourmetKlub.java
@@ -1,10 +1,15 @@
 package co.hodler;
 
 import static spark.Spark.get;
+
+import spark.Spark;
+
 import static co.hodler.infrastructure.JsonReader.*;
 
 public class GourmetKlub {
   public static void main(String[] args) {
+    Spark.port(Integer.valueOf(System.getenv("PORT")));
+
     get("/restaurants", (req, res) -> jsonArrayAsString("./src/main/webapp/data/restaurants.json"));
   }
 }
