357fa8d0593f0717f32e5b3ad2c21174c1c4971c
xorrr <derxor@gmail.com>
2015-07-19 21:19:09 +0200

    provide hardcoded list of restaurants via rest

--- src/main/java/co/hodler/GourmetKlub.java
+++ src/main/java/co/hodler/GourmetKlub.java
@@ -1,9 +1,10 @@
 package co.hodler;
 
 import static spark.Spark.get;
+import static co.hodler.infrastructure.JsonReader.*;
 
 public class GourmetKlub {
   public static void main(String[] args) {
-    get("/hello", (req, res) -> "Hello World");
+    get("/restaurants", (req, res) -> jsonArrayAsString("./src/main/webapp/data/restaurants.json"));
   }
 }
