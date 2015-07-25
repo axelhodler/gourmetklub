8bdbccb1b21b338c6f9ea579c7777ad04169ddc1
xorrr <derxor@gmail.com>
2015-07-25 21:21:30 +0200

    create extra folder for integration tests

---
+++ src/it/java/co/hodler/SampleIT.java
@@ -0,0 +1,13 @@
+package co.hodler;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleIT {
+
+  @Test
+  public void testName() throws Exception {
+    assertTrue(true);
+  }
+}
