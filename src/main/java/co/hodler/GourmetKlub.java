c0e67bed8589a9cb92a90889248fee35f8479c21
xorrr <derxor@gmail.com>
2015-09-10 21:11:15 +0200

    add access control allow origin header to restaurants response

--- src/main/java/co/hodler/GourmetKlub.java
+++ src/main/java/co/hodler/GourmetKlub.java
@@ -10,6 +10,9 @@ public class GourmetKlub {
   public static void main(String[] args) {
     Spark.port(Integer.valueOf(System.getenv("PORT")));
 
-    get("/restaurants", (req, res) -> jsonObjectAsString("./src/main/webapp/data/restaurants.json"));
+    get("/restaurants", (req, res) -> {
+      res.header("Access-Control-Allow-Origin", "*");
+      return jsonObjectAsString("./src/main/webapp/data/restaurants.json");
+    });
   }
 }
