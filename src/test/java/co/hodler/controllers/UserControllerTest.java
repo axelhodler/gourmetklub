07e61efebb6d95b1ec0eac4c30d422fe90aba6ad
xorrr <derxor@gmail.com>
2015-09-20 19:26:01 +0200

    master request and response will have to be mocked

--- src/test/java/co/hodler/controllers/UserControllerTest.java
+++ src/test/java/co/hodler/controllers/UserControllerTest.java
@@ -1,20 +1,32 @@
 package co.hodler.controllers;
 
+import static org.mockito.Mockito.verify;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import co.hodler.actions.RegisterUser;
+import spark.Request;
+import spark.Response;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
 
   @Mock
   RegisterUser registerUser;
+  @Mock
+  Request request;
+  @Mock
+  Response response;
 
   @Test
   public void registerUser() {
     UserController c = new UserController(registerUser);
+
+    c.registerUser(request, response);
+
+    verify(registerUser).register(null);
   }
 }
