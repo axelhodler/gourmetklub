61b62c77e115aeda65b71ed542fc6ecbfdc3fd74
xorrr <derxor@gmail.com>
2015-10-17 18:18:00 +0200

    user controller registers user

--- src/main/java/co/hodler/controllers/UserController.java
+++ src/main/java/co/hodler/controllers/UserController.java
@@ -1,19 +1,22 @@
 package co.hodler.controllers;
 
 import co.hodler.actions.RegisterUser;
+import co.hodler.model.UserDto;
 import spark.Request;
 import spark.Response;
 
 public class UserController {
 
   private RegisterUser registerUser;
+  private UserDto dto;
 
-  public UserController(RegisterUser registerUser) {
+  public UserController(RegisterUser registerUser, UserDto dto) {
     this.registerUser = registerUser;
+    this.dto = dto;
   }
 
   public String registerUser(Request request, Response response) {
-    registerUser.register(null);
+    registerUser.register(dto.fromJson(request.body()));
     return null;
   }
 }
--- src/test/java/co/hodler/controllers/UserControllerTest.java
+++ src/test/java/co/hodler/controllers/UserControllerTest.java
@@ -1,15 +1,22 @@
 package co.hodler.controllers;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.verify;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import co.hodler.actions.RegisterUser;
+import co.hodler.model.HashedPassword;
+import co.hodler.model.User;
+import co.hodler.model.UserDto;
 import spark.Request;
-import spark.Response;
 
 @RunWith(MockitoJUnitRunner.class)
 public class UserControllerTest {
@@ -18,15 +25,22 @@ public class UserControllerTest {
   RegisterUser registerUser;
   @Mock
   Request request;
-  @Mock
-  Response response;
+  @Captor
+  ArgumentCaptor<User> user;
+
+  private static final String USER_JSON = "{\"name\":\"johnny\", \"mail\":\"john@john.org\", \"password\":\"johnny123\"}";
 
   @Test
   public void registerUser() {
-    UserController c = new UserController(registerUser);
+    given(request.body()).willReturn(USER_JSON);
+    UserController c = new UserController(registerUser, new UserDto());
 
-    c.registerUser(request, response);
+    c.registerUser(request, null);
 
-    verify(registerUser).register(null);
+    verify(registerUser).register(user.capture());
+    assertThat(user.getValue().getName(), is("johnny"));
+    assertThat(user.getValue().getMail(), is("john@john.org"));
+    assertThat(user.getValue().getHashedPassword(),
+        is(new HashedPassword("johnny123")));
   }
 }
