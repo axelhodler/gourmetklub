8f894577bb7a6d776afa744fd0aa3d7e0284577c
xorrr <derxor@gmail.com>
2015-10-18 19:54:56 +0200

    provide coordinates via rest

--- src/main/java/co/hodler/GourmetKlub.java
+++ src/main/java/co/hodler/GourmetKlub.java
@@ -2,6 +2,10 @@ package co.hodler;
 
 import static spark.Spark.get;
 
+import co.hodler.actions.ProvideCoordinates;
+import co.hodler.controllers.CoordinatesController;
+import co.hodler.infrastructure.external.GoogleMapsCoordinatesProvider;
+import co.hodler.model.CoordinatesDto;
 import spark.Spark;
 
 import static co.hodler.infrastructure.JsonReader.*;
@@ -13,6 +17,12 @@ public class GourmetKlub {
     get("/restaurants", (req, res) -> {
       res.header("Access-Control-Allow-Origin", "*");
       return jsonObjectAsString("./src/main/webapp/data/restaurants.json");
+    });
+
+    get("/coordinates", (req, res) -> {
+      ProvideCoordinates provideCoordinates = new ProvideCoordinates(new GoogleMapsCoordinatesProvider());
+      CoordinatesController controller = new CoordinatesController(provideCoordinates, new CoordinatesDto());
+      return controller.requestCoords(req, res);
     });
   }
 }
